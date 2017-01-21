82448eb17a33d97adc26266572e74951f1e1e6a3
itotallyrock <itotallyrock@users.noreply.github.com>
2017-01-21 15:42:31 -0600

    Added permission definitions

--- web/middleware/permissionHandler.js
+++ web/middleware/permissionHandler.js
@@ -2,7 +2,30 @@
 const permission = require('permission')
 
 let handler = () => {}
-
+/**
+  * @apiDefine student student
+  * Student role with few admin features
+*/
+/**
+  * @apiDefine tester tester
+  * Application tester role with features needed for testing
+*/
+/**
+  * @apiDefine teacher teacher
+  * Teacher role for micromanaging the public side of the application
+*/
+/**
+  * @apiDefine stuco stuco
+  * Student council role allowing managing for maintaining the public side of the application
+*/
+/**
+  * @apiDefine developer developer
+  * Developer role with features any feature needed to modify or continue progress on the public or private application
+*/
+/**
+  * @apiDefine admin admin
+  * Admin role with any permissions required for running the application
+*/
 let after = (req, res, next, authStatus) => {
   if (authStatus === permission.AUTHORIZED) {
     return next()
