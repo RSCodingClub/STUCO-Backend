88ea62e96b9493b4657c3939220c664c5a929ccf
itotallyrock <itotallyrock@users.noreply.github.com>
2017-01-31 22:40:06 -0600

    Deleted old test files

--- test/models/Badge.js
+++
@@ -1,9 +0,0 @@
-const {expect} = require('chai');
-const Badge = require(__dirname.replace('test', 'app') + '/Badge.model');
-
-describe('Badge Model', function () {
-	describe('Schema', function () {
-		let badge = new Badge({});
-		expect(badge).to.not.be.undefined;
-	});
-});
